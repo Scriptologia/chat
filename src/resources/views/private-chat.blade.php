874491385d682f045169683c3b54fec8245edb2d
Алексей <alexy.74@mail.ru>
2022-12-28 21:07:42 +0200

    удалил файлы

--- src/resources/views/private-chat.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout')
-@section('content')
-<div class="container">
-  <private-chat ></private-chat>
-</div>
-@endsection
--- src/resources/views/room.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-@section('content')
-<div class="container">
-  @if(Auth::check())
-  <private-chat :room="{{$room}}" :user="{{Auth::user()}}"></private-chat>
-    @endif
-</div>
-@endsection
